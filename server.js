f0287f9ffcd64194c07ea484bc48b4800c360982
vania.catorceno <vania.catorceno@fundacion-jala.org>
2016-08-01 22:17:58 -0500

    added procfile for heroku

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');				// Utilizamos express
 var morgan = require('morgan');
 var mongoose = require('mongoose'); 				// mongoose para mongodb
-var port  	 = process.env.PORT || 9000; 			// Cogemos el puerto 8080
+var port  	 = process.env.PORT || 3000; 
+var ip = process.env.IP || 127.0.0.1			// Cogemos el puerto 8080
 
 
 // Configuracion
 mongoose.connect('mongodb://localhost:27017/MainProject'); 	// Hacemos la conexión a la base de datos 
-//de Mongo con nombre "MeanExample"
 
 app.use(express.static(__dirname + '/angular')); 		
 app.use(morgan('dev')); 			// activamos el log en modo 'dev'
@@ -22,5 +22,5 @@ app.use(methodOverride());
 require('./routes/mainRoute.js')(app);
 
 // Cogemos el puerto para escuchar
-app.listen(port);
-console.log("APP por el puerto " + port);
+app.listen(port, ip);
+console.log("APP por el puerto " + port + ":"+ ip);
